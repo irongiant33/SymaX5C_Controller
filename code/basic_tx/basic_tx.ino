003abf10a388b6787026112ed73f62104d51506a
irongiant <samuelmiller0714@gmail.com>
2021-08-03 21:53:43 -0400

    end of day commit

--- code/basic_tx/basic_tx.ino
+++ code/basic_tx/basic_tx.ino
@@ -1,14 +1,33 @@
 #include <SPI.h> //https://www.arduino.cc/en/reference/SPI
 
 //these define what pin each signal is connected to on the arduino
-#define CE 10 //slave select
-#define SCK 13
-#define MOSI 11
-#define MISO 12
-#define CSN 9 //not controlled by SPI library
+#define CE    10  //slave select
+#define SCK   13
+#define MOSI  11
+#define MISO  12
+#define CSN   9   //not controlled by SPI library
 
 //SPI constants
 #define SPI_SPEED 10000000 //this is defined to be the limit of SPI on p.47 of the NRF specification
+
+//command constants
+#define R_RX_PAYLOAD        0b01100001
+#define W_TX_PAYLOAD        0b10100000
+#define FLUSH_TX            0b11100001
+#define FLUSH_RX            0b11100010
+#define REUSE_TX_PL         0b11100011
+#define R_RX_PL_WID         0b01100000
+#define W_TX_PAYLOAD_NOACK  0b10110000
+#define NOP                 0b11111111
+
+//other constants
+#define MAX_NUM_REG_READS   100   //the maximum number of registers to read
+#define SERIAL_DATA_RATE    9600
+#define NIBBLE_SIZE         4     //num bits in one nibble
+#define NUM_BITS_IN_BYTE    8
+
+//global variables
+char hex_vals[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
 void setup()
 {
@@ -18,7 +37,7 @@ void setup()
   SPI.setDataMode(SPI_MODE0);
   pinMode(CSN, OUTPUT);
   pinMode(CE, OUTPUT);
-  Serial.begin(9600);
+  Serial.begin(SERIAL_DATA_RATE);
 
   //initial pin settings
   digitalWrite(CSN, HIGH);
@@ -30,24 +49,82 @@ void loop()
   //according to spec: "Change to active modes only happens if CE is set high and
   //when CE is set low, the nRF24L01 returns to standby-I mode.
   digitalWrite(CE, HIGH);
+  unsigned int * reg_value = calloc(MAX_NUM_REG_READS, sizeof(int)); //declaration of variable to store register values
+  int counter = 0; //will count how many registers we have read
   
   //write to register
   digitalWrite(CSN, LOW);
-  SPI.transfer(0b00101111); //001 (write) 01111 (0x0F)
-  SPI.transfer(0b01010111); //LSB of address for Data Pipe 5
-  delay(100);
+  reg_value[counter] = SPI.transfer(0b00101111); //001 (write) 01111 (0x0F)
+  counter++;
+  reg_value[counter] = SPI.transfer(0b01010111); //LSB of address for Data Pipe 5
+  counter++;
   digitalWrite(CSN, HIGH);
-  delay(100);
 
   //read from the same register
   digitalWrite(CSN, LOW);
-  int reg_value = SPI.transfer(0b00001111); //000 (read) 01111 (0x0F)
-  //Serial.print(reg_value);
-  SPI.end(); //https://www.arduino.cc/en/Reference/SPIEnd
-  delay(100);
+  reg_value[counter] = SPI.transfer(0b00001111); //000 (read) 01111 (0x0F)
+  counter++;
+  reg_value[counter] = SPI.transfer(NOP); //we are just triggering the SCK to read the actual register contents
+  counter++;
   digitalWrite(CSN, HIGH);
-  delay(100);
 
-  //reset the CE pin
+  //read from TX_ADDR register
+  digitalWrite(CSN, LOW);
+  reg_value[counter] = SPI.transfer(0b00010000); //000 (read) 00111 (0x10)
+  counter++;
+  char * tx_addr = calloc(5, sizeof(char));
+   //read 5 bytes (the value that we transfer over MOSI doesn't matter, we just want SCK to trigger MISO read)
+  for(int i = 0; i < 5; i++)
+  {
+    tx_addr[i] = SPI.transfer(NOP);
+  }
+  digitalWrite(CSN, HIGH);
+
+  //print results
+  for(int i = 0; i < counter; i++)
+  {
+    print_bytes(reg_value[i], true);
+  }
+  for(int i = 0; i < 5; i++)
+  {
+    print_bytes((int) tx_addr[i], false);
+  }
+
+  //reset the CE pin and terminate SPI
   digitalWrite(CE, LOW);
+  SPI.end(); //https://www.arduino.cc/en/Reference/SPIEnd
+  free(reg_value);
+  free(tx_addr);
+}
+
+void print_bytes(unsigned int value, bool print_binary)
+{
+  unsigned int mask_value;
+  int num_bits = sizeof(value) * NUM_BITS_IN_BYTE; //convert # bytes to # bits
+  if(print_binary)
+  {
+    mask_value = pow(2, num_bits - 1);
+    Serial.print("0b");
+    for(unsigned int i = 0; i < num_bits; i++)
+    {
+      int result = (value << i) & mask_value;
+      Serial.print((result >> (num_bits - 1)) & 1);
+    }
+  }
+  else
+  {
+    int num_nibbles = sizeof(value) * (NUM_BITS_IN_BYTE/NIBBLE_SIZE); //convert # bytes to # nibbles
+    for(int i = 0; i < NIBBLE_SIZE; i++)
+    {
+      mask_value += pow(2, num_bits - (i + 1));
+    }
+    Serial.print("0x");
+    for(unsigned int i = 0; i < num_nibbles; i++)
+    {
+      int result = (value << i * NIBBLE_SIZE) & mask_value;
+      Serial.print(hex_vals[(result >> (num_bits - NIBBLE_SIZE)) & 15]);
+    }
+  }
+  Serial.println();
+  return;
 }
--- notes.md
+++ notes.md
@@ -1,4 +1,4 @@
-## Syma X5C
+## ASyma X5C
 
 ## Resources
 
@@ -31,4 +31,60 @@
 - Connecting the PicoScope to the output of the Arduino to see if it is actually doing any SPI signaling. So far, there have been no good results. 
 - Connected LED to CSN pin and added some delays around setting the output to HIGH/LOW so that I could see the LED blink on and off. It did just that, but there was still no output on the PicoScope, even on the D3 channel. I must've configured somethign incorrectly with the scope. 
   - needed to modify the trigger. it was set to "rapid" but after moving it to "none" I was able to see the digital signal.
-  - to properly view the digital signals, I would suggest lowering their threshold from the "select digital channels/groups" menu. Even though the signals are supposed to be 5V, selecting 3V ensures good looking digital waveforms.
+  - to properly view the digital signals, I would suggest lowering their threshold from the "select digital channels/groups" menu. Even though the signals are supposed to be 5V peak-to-peak, selecting 3V ensures good looking digital waveforms.
+- Managed to get some data with the PicoScope. There is a git commit on today's date that has the Arduino code that performs this, but I've copied it below for redundancy. This transfer occurs very fast, so you have to set a trigger in PicoScope to capture it. I decided to trigger when the digital signal D3 (in my case the Chip Select Not signal) was falling AND when the D0 (MOSI data) was HIGH. Then, I started capturing and then uploaded the code to the Arduino. The capture stopped and in the separate PicoScope window for "serial decoding" I was able to see the exact two bytes that I sent out via `SPI.transfer()`. 
+  - In the PicoScope software, you can only have one condition checked for each digital signal. You can also only have one condition that is rising or falling for __all__ digital signals. However, you may have as many digital signals checked for either HIGH or LOW as you please.
+
+```c
+#include <SPI.h> //https://www.arduino.cc/en/reference/SPI
+
+//these define what pin each signal is connected to on the arduino
+#define CE 10 //slave select, not controlled by SPI library
+#define SCK 13
+#define MOSI 11
+#define MISO 12
+#define CSN 9 //not controlled by SPI library
+
+//SPI constants
+#define SPI_SPEED 10000000 //this is defined to be the limit of SPI on p.47 of the NRF specification
+
+void setup()
+{
+  //initializations
+  SPI.begin(); //https://www.arduino.cc/en/Reference/SPIBegin
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE0);
+  pinMode(CSN, OUTPUT);
+  pinMode(CE, OUTPUT);
+  Serial.begin(9600);
+
+  //initial pin settings
+  digitalWrite(CSN, HIGH);
+  digitalWrite(CE, LOW);
+}
+
+void loop()
+{
+  //according to spec: "Change to active modes only happens if CE is set high and
+  //when CE is set low, the nRF24L01 returns to standby-I mode.
+  digitalWrite(CE, HIGH);
+  
+  //write to register
+  digitalWrite(CSN, LOW);
+  SPI.transfer(0b00101111); //001 (write) 01111 (0x0F)
+  SPI.transfer(0b01010111); //LSB of address for Data Pipe 5
+  digitalWrite(CSN, HIGH);
+
+  //read from the same register
+  digitalWrite(CSN, LOW);
+  int reg_value = SPI.transfer(0b00001111); //000 (read) 01111 (0x0F)
+  digitalWrite(CSN, HIGH);
+  SPI.end(); //https://www.arduino.cc/en/Reference/SPIEnd
+
+  //reset the CE pin
+  digitalWrite(CE, LOW);
+}
+```
+
+
+
